40ad36da24cd3996909a3fcd6fa96536875ae5c8
gitterxo <dorinenache@fastmail.com>
2018-07-03 17:40:04 +0300

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -115,6 +115,21 @@ app.get('/users/me', authenticate, (req, res) => { // authenticate pus aici inse
     res.send(req.user);
 });
 
+// POST /users/login {email, password)
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        user.generateAuthToken().then((token) => {
+            return res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    })
+
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
