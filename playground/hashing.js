3e0d5468e079f85051949e3071547a1b79a1375d
gitterxo <dorinenache@fastmail.com>
2018-07-03 17:00:56 +0300

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = 'password1';
 
-var token = jwt.sign(data,'123abc'); // al doilea param e secretul
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     })
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword = "$2a$10$gcAo64UEmy/qhPVbZJi3kOasHgGsKPuaboKu.AhrQpxHUo01Ejl3.";
+
+bcrypt.compare(password,hashedPassword, (err, result) => {
+    console.log(result);
+});
+
+// var data = {
+//     id: 10
+// };
+//
+// var token = jwt.sign(data,'123abc'); // al doilea param e secretul
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 
 // var message = "I am user number 3";
