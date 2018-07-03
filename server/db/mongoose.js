8adc76e56a4fc225b091bcc8d615e35a5071f83a
gitterxo <dorinenache@fastmail.com>
2018-07-03 11:20:16 +0300

    setup app for keroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // ca sa foloseasca sist default de promise in loc de callback
-mongoose.connect('mongodb://localhost:27017/TodoApp'); // pastreaza conexiunea spre deosebit de modulul default
+//mongoose.connect('mongodb://localhost:27017/TodoApp'); // pastreaza conexiunea spre deosebit de modulul default
+ongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'); // pt heroku
 
 module.exports = {
     mongoose: mongoose
